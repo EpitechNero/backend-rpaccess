2a39b77f686e5803e4140640188ea0bd7cc1a135
GILLIERS Renaud <GILLIERSR@exploit-fr.water.groupve.local>
2025-05-05 15:01:12 +0200

    [+] logs drive method

--- api/services/driveService.js
+++ api/services/driveService.js
@@ -3,13 +3,30 @@ const tasks = {};
 const generateUniqueId = require('../utils/generateId');
 
 async function copyDriveFile(fileId, targetFolderId, filename) {
-  const drive = google.drive({ version: 'v3', auth: oAuth2Client });
-  const response = await drive.files.copy({
-    fileId,
-    resource: { parents: [targetFolderId], name: filename },
-    supportsAllDrives: true,
-  });
-  return response.data.id;
+  try {
+    const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+    const response = await drive.files.copy({
+      fileId,
+      resource: {
+        parents: [targetFolderId],
+        name: filename,
+      },
+      supportsAllDrives: true,
+    });
+
+    logger.info('📁 Fichier copié dans Google Drive', {
+      newFileId: response.data.id,
+      filename,
+      taskId,
+    });
+
+    return response.data.id;
+  } catch (error) {
+    logger.error('❌ Erreur lors de la copie du fichier dans Google Drive', {
+      error: error.response?.data || error.message,
+    });
+    throw error;
+  }
 }
 
 module.exports = { tasks, generateUniqueId, copyDriveFile };
