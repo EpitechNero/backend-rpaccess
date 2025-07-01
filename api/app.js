5c3a531f8eac621c302b6fc630893017b4436814
Aleksyc <alexis.cocquerel@gmail.com>
2025-07-01 09:10:14 +0200

    [~] fix initial commit documentation

--- api/app.js
+++ api/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const bodyParser = require('body-parser');
 const YAML = require('yamljs');
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 
 const zendeskRoutes = require('./routes/zendeskRoutes');
 const driveRoutes = require('./routes/driveRoutes');
 const automationRoutes = require('./routes/automationRoutes');
 // const psqlRoutes = require('./routes/psqlRoutes');
-
 const requestLogger = require('./middlewares/requestLogger');
 
 const app = express();
@@ -25,7 +26,7 @@ app.use(requestLogger);
 app.use('/zendesk', zendeskRoutes);
 app.use('/drive', driveRoutes);
 app.use('/aa', automationRoutes);
-//app.use('/db', psqlRoutes);
+// app.use('/db', psqlRoutes);
 
 app.get('/', (req, res) => res.send('Its Home'));
 
