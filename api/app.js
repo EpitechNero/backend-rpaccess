cd56e6b9058a5d7a3f3af65f5af74a71d94bbc7d
Aleksyc <alexis.cocquerel@gmail.com>
2025-07-01 08:54:17 +0200

    [+] initial commit documentation

--- api/app.js
+++ api/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const YAML = require('yamljs');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const zendeskRoutes = require('./routes/zendeskRoutes');
 const driveRoutes = require('./routes/driveRoutes');
 const automationRoutes = require('./routes/automationRoutes');
-const psqlRoutes = require('./routes/psqlRoutes');
+// const psqlRoutes = require('./routes/psqlRoutes');
 
 const requestLogger = require('./middlewares/requestLogger');
 
@@ -14,11 +18,14 @@ app.use(cors({ origin: ['https://fr-ist-isteau-rpaccef.web.app', 'http://localho
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Documentation Swagger
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(requestLogger);
 app.use('/zendesk', zendeskRoutes);
 app.use('/drive', driveRoutes);
 app.use('/aa', automationRoutes);
-app.use('/db', psqlRoutes);
+//app.use('/db', psqlRoutes);
 
 app.get('/', (req, res) => res.send('Its Home'));
 
