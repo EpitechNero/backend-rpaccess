b0772c3bbb4727cbfc64122a1d647ca2f027dbd6
Aleksyc <alexis.cocquerel@gmail.com>
2025-07-15 10:50:23 +0200

    [+] Verification Auth JWT

--- api/middlewares/jwtMiddleware.js
+++ api/middlewares/jwtMiddleware.js
@@ -7,7 +7,40 @@ async function authMiddleware(req, res, next) {
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({ message: 'Token manquant' });
     }
-    
+
+    const idToken = authHeader.split('Bearer ')[1];
+
+    try {
+        const decodedHeader = JSON.parse(
+            Buffer.from(idToken.split('.')[0], 'base64').toString()
+        );
+        const kid = decodedHeader.kid;
+
+        if (!kid) {
+            return res.status(401).json({ message: 'Token invalide : kid manquant' });
+        }
+
+        const { data: keys } = await axios.get('https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com');
+        const publicKey = keys[kid];
+
+        if (!publicKey) {
+            return res.status(401).json({ message: 'Clé publique non trouvée pour ce kid' });
+        }
+
+        const decoded = jwt.verify(idToken, publicKey, {
+            algorithms: ['RS256'],
+            issuer: 'https://securetoken.google.com/fr-ist-isteau-rpaccef',
+            audience: 'fr-ist-isteau-rpaccef'
+        });
+
+        req.user = decoded;
+
+        console.log('Utilisateur authentifié:', req.user);
+
+    } catch (error) {
+        return res.status(401).json({ message: 'Token invalide', error: error.message });
+    }
+
     next();
 }
 
