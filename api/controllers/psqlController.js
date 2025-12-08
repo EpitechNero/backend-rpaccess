71fabf87a1f1f076a6bb94b2623131fc6d198171
GILLIERS Renaud <GILLIERSR@exploit-fr.water.groupve.local>
2025-12-08 11:39:57 +0100

    [+] correction insertion activités

--- api/controllers/psqlController.js
+++ api/controllers/psqlController.js
@@ -367,7 +367,7 @@ exports.getTopUsers = async (req, res) => {
 exports.setActivity = async (req, res) => {
   logger.info('📥 Requête reçue pour insertActivity :', JSON.stringify(req.body));
   try {
-    const result = await insertActivity();
+    const result = await insertActivity(req.body);
     res.status(201).json({ message: 'Activité insérée avec succès', data: result });
   } catch (error) {
     res.status(500).json({
